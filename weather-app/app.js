21fc3b0d0e8ad7eee4219bdb334998f62a8eab81
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-01-12 19:22:46 -0800

    require yargs, add user input for address

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,28 @@
 const request = require('request');
 const mapquest_key = require('./config.js').mapquest_key;
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe:'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+console.log(argv);
+
+var encoded_address = encodeURIComponent(argv.address);
 
 request({
-  url: `https://www.mapquestapi.com/geocoding/v1/address?key=${mapquest_key}&location=1301%20lombard%20street%20philadelphia`,
+  url: `https://www.mapquestapi.com/geocoding/v1/address?key=${mapquest_key}&location=${encoded_address}`,
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(`Address: ${body.results[0].locations[0].latLng.lng}`);
+  console.log(`Address: ${body.results[0].locations[0].latLng.lat}`);
 });
