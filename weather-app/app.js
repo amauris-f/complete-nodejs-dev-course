515d774d8d8a5377428c04f703448c9ab0e133e9
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-01-12 19:43:02 -0800

    handle callback errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -23,6 +23,13 @@ request({
   url: `https://www.mapquestapi.com/geocoding/v1/address?key=${mapquest_key}&location=${encoded_address}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].locations[0].latLng.lng}`);
-  console.log(`Address: ${body.results[0].locations[0].latLng.lat}`);
+  console.log(JSON.stringify(response, undefined, 2));
+  if(error){
+    console.log('Unable to connect to Mapquest servers.');
+  }else if (!body || body.results[0].locations.length === 0){
+    console.log("Unable to find that address");
+  }else {
+    console.log(`Address: ${body.results[0].locations[0].latLng.lng}`);
+    console.log(`Address: ${body.results[0].locations[0].latLng.lat}`);
+  }
 });
