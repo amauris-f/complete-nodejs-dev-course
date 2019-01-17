8f5bce100ad79eeeb8d2412f9a6a289bc62b74be
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-01-16 17:56:31 -0800

    return array of users in new route

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -3,7 +3,25 @@ const express = require('express');
 var app = express();
 
 app.get('/',(req, res)=> {
-  res.send('Hello World');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo app v1.0'
+  });
 });
 
+app.get('/users', (req, res)=> {
+  res.send([
+    {
+      name: 'Amauris',
+      age: 23
+    },
+    {
+      name: 'John',
+      age: 28
+    }
+  ]);
+})
+
 app.listen(3000);
+
+module.exports.app = app;
