6fe94a49a0b2cbefa6ab912c4f1dd0c9f708fcae
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-01-18 08:39:17 -0800

    add new GET requests

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectId} = require('mongodb');
 
 var app = express();
 
@@ -21,9 +22,36 @@ app.post('/todos', (req,res) => {
   });
 });
 
+app.get('/todos', (req,res) => {
+  Todo.find().then((todos)=> {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id; 
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo)=> {
+    if(!todo){
+      return res.status(404).send();
+    }
+      res.send({todo});
+  }).catch((err) => {
+    res.status(404).send();
+  });  
+});
 
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
 
+
+module.exports = {
+  app
+}
