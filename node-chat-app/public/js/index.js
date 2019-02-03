faa8b97b518eb02c601b48dc73b298e3c92a917a
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-02-03 12:11:42 -0800

    scroll to bottom if user is close to bottom

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -1,4 +1,19 @@
 var socket = io();
+
+function scrollToBottom(){
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight ){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function(){
   console.log('Connected to server');
 });
@@ -12,10 +27,6 @@ socket.on('newEmail', function(email){
 }); 
 
 socket.on('newMessage', function(message){
-  // var formatted = moment(message.createdAt).format('h:mm a');
-  // var li = jQuery('<li></li>');
-  // li.text(`${message.from} ${formatted}: ${message.text}`);
-  // jQuery('#messages').append(li);
   var formattedTime = moment(message.createdAt).format('h:mm a');
   var template = jQuery('#message-template').html();
   var html = Mustache.render(template, {
@@ -25,15 +36,10 @@ socket.on('newMessage', function(message){
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message){
-  // var formatted = moment(message.createdAt).format('h:mm a');
-  // var li = jQuery('<li></li>');
-  // var a = jQuery('<a target="_blank">My current location</a>');
-  // li.text(`${message.from} ${formatted}: `);
-  // a.attr('href', message.url);
-  // li.append(a);
   var formattedTime = moment(message.createdAt).format('h:mm a');
   var template = jQuery('#location-message-template').html();
   html = Mustache.render(template, {
@@ -42,6 +48,7 @@ socket.on('newLocationMessage', function(message){
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e){
