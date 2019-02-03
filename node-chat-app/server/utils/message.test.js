ee2642d5c1647cd5790eba1ae2871885a2ad85ef
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-02-02 18:22:44 -0800

    add geolocation support via geolocation api

--- node-chat-app/server/utils/message.test.js
+++ node-chat-app/server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', ()=>{
   it('should generate correct message object', () => {
@@ -8,6 +8,20 @@ describe('generateMessage', ()=>{
     var message = generateMessage(from, text);
     
     expect(message).toInclude({from, text});
+    expect(message.createdAt).toBeA('number'); 
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Admin';
+    var lat = 1;
+    var long = 2;
+    var message = generateLocationMessage(from, lat, long);
+    expect(message).toInclude({
+        from: from,
+        url: 'https://www.google.com/maps?q=1,2'
+    });
     expect(message.createdAt).toBeA('number'); 
   });
 });
