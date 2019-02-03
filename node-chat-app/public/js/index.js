08880cf774d0fe198b9f0b9a663cf86698d72fd5
Amauris Ferreira <ar.ferreira17@gmail.com>
2019-02-03 10:19:00 -0800

    Format timestamps using moment.js

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -12,16 +12,17 @@ socket.on('newEmail', function(email){
 }); 
 
 socket.on('newMessage', function(message){
-  console.log('newMessge',message);
+  var formatted = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formatted}: ${message.text}`);
   jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message){
+  var formatted = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current location</a>');
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formatted}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
